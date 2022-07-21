a88c13169737b6a65c4d0bc18cdc29e59de4b56a
renatocandc <renato.hysa@carandclassic.com>
2022-07-21 15:10:34 +0200

    Fix navigation

--- resources/js/components/ResourceNavigation.vue
+++ resources/js/components/ResourceNavigation.vue
@@ -19,31 +19,55 @@
         <div class="hidden sm:block">
             <div class="border-b border-gray-200">
                 <nav class="-mb-px flex space-x-8" aria-label="Tabs">
-                    <span class="border-indigo-500 text-indigo-600 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm cursor-pointer"
-                        @click="$emit('changeNav', 'pdfs')">
+                    <span id="pdfs" class="border-indigo-500 text-indigo-600 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm cursor-pointer"
+                        @click="makeCurrentNav('pdfs')">
                         PDFs
 
                         <span class="bg-indigo-100 text-indigo-600 hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">4</span>
                     </span>
 
-                    <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200" -->
-                    <span class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm cursor-pointer"
-                       @click="$emit('changeNav', 'snippets')">
+                    <span id="snippets" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm cursor-pointer"
+                       @click="makeCurrentNav('snippets')">
                         Snippets
                         <span class="bg-gray-100 text-gray-900 hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">52</span>
                     </span>
 
-                    <a href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm">
-                        Phone Screening
+                    <span id="links" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm cursor-pointer"
+                      @click="makeCurrentNav('links')">
+                        Links
 
                         <span class="bg-gray-100 text-gray-900 hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">6</span>
-                    </a>
-
-                    <a href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm"> Offer </a>
-
-                    <a href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm"> Disqualified </a>
+                    </span>
                 </nav>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentNav: 'pdfs',
+      oldNav: null,
+    }
+  },
+
+  methods: {
+    makeCurrentNav(nav) {
+      this.$emit('changeNav', nav)
+
+      this.oldNav = this.currentNav
+      this.currentNav = nav
+
+      let oldNav = document.getElementById(this.oldNav)
+      oldNav.classList.remove('border-indigo-500', 'text-indigo-600')
+      oldNav.classList.add('border-transparent', 'text-gray-500', 'hover:text-gray-700', 'hover:border-gray-200')
+
+      let currentNav = document.getElementById(this.currentNav)
+      currentNav.classList.remove('border-transparent', 'text-gray-500', 'hover:text-gray-700', 'hover:border-gray-200')
+      currentNav.classList.add('border-indigo-500', 'text-indigo-600')
+    }
+  }
+}
+</script>
